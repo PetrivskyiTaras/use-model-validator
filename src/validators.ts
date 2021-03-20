6863a20761312945c76191f604cc628a3753466a
petrivskyitaras <taras02pv@gmail.com>
2021-03-20 17:58:15 +0200

    Add validators, add new descriptions to README.md

---
+++ src/validators.ts
@@ -0,0 +1,30 @@
+export const requireValidator = (errorText: string, allowZero = true) => (value: string | number): string => {
+    return typeof value === 'undefined' || value === '' || value === null || (!allowZero && value === 0) ? errorText : '';
+};
+
+export const maxLengthValidator = (max: number, errorText: string) => (value: string | number): string => {
+    if (typeof value === 'number') {
+        return value > max ? errorText : '';
+    }
+    return (value || '').length > max ? errorText : '';
+};
+
+export const minLengthValidator = (min: number, errorText: string) => (value: string | number): string => {
+    if (typeof value === 'number') {
+        return value < min ? errorText : '';
+    }
+    return (value || '').length < min ? errorText : '';
+};
+
+export const regExpValidator = (regx: RegExp, errorText: string) => (value: string): string => {
+    const arr = value.match(regx);
+    return !!arr && arr.length > 0 ? '' : errorText;
+};
+
+export const emailValidator = (errorText: string) => (email: string): string => {
+    return !/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g.test(
+        email
+    ) && email
+        ? errorText
+        : '';
+};
